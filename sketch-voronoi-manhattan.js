77bb9bec8081392acf43494806dbd81898f4c728
mlknz <rkdshe@gmail.com>
2020-06-22 03:22:55 +0300

    Improve inner cell generation
    Self-intersections WIP

--- sketch-voronoi-manhattan.js
+++ sketch-voronoi-manhattan.js
@@ -35,6 +35,18 @@ const manhDist = (a, b) => {
     return Math.abs(b[0] - a[0]) + Math.abs(b[1] - a[1]);
 };
 
+const manhPerimeter = (poly) => {
+    let result = 0;
+    for (let i = 1; i < poly.length; ++i)
+    {
+        const a = poly[i];
+        const b = poly[i - 1];
+        result += manhDist(a, b);
+    }
+    result += manhDist(poly[0], poly[poly.length - 1]);
+    return result;
+};
+
 const generateManhattanVoronoi = (width, height) => {
     const randomPoints = generatePoints(randomPointsCount, width, height, margin);
     const manhattan = generateL1Voronoi(randomPoints, width, height, true);
@@ -97,6 +109,102 @@ const findMedianDir = (a, b, c) => {
     return normalize([ba[0] + bc[0], ba[1] + bc[1]]);
 };
 
+const cross2d = (v, w) => {
+    return v[0] * w[1] - v[1] * w[0];
+};
+
+const intersectEdges = (a, b) => {
+    const a0x = a[0][0];
+    const a1x = a[1][0];
+    const a0y = a[0][1];
+    const a1y = a[1][1];
+    const b0x = b[0][0];
+    const b1x = b[1][0];
+    const b0y = b[0][1];
+    const b1y = b[1][1];
+
+    const aMinX = Math.min(a0x, a1x);
+    const aMaxX = Math.max(a0x, a1x);
+    const aMinY = Math.min(a0y, a1y);
+    const aMaxY = Math.max(a0y, a1y);
+    const bMinX = Math.min(b0x, b1x);
+    const bMaxX = Math.max(b0x, b1x);
+    const bMinY = Math.min(b0y, b1y);
+    const bMaxY = Math.max(b0y, b1y);
+    if (aMaxX <= bMinX || aMinX >= bMaxX || aMaxY <= bMinY || aMinY >= bMaxY)
+    {
+        return false;
+    }
+    const aDir = findDiffDir(a[0], a[1]);
+    const bDir = findDiffDir(b[0], b[1]);
+
+    const abDot = aDir[0] * bDir[0] + aDir[1] * bDir[1];
+    if (Math.abs(abDot) > 0.999)
+    {
+        return false;
+    }
+
+    const aDirUnnorm = [a1x - a0x, a1y - a0y];
+    const bDirUnnorm = [b1x - b0x, b1y - b0y];
+    const t = cross2d([b0x - a0x, b0y - a0y], bDirUnnorm) / cross2d(aDirUnnorm, bDirUnnorm);
+    const u = cross2d([a0x - b0x, a0y - b0y], aDirUnnorm) / cross2d(bDirUnnorm, aDirUnnorm);
+    if (isNaN(t) || isNaN(u) || t < 0 || t > 1 || u < 0 || u > 1)
+    {
+        return false;
+    }
+    return [a0x + aDirUnnorm[0] * t, a0y + aDirUnnorm[1] * t];
+};
+
+const fixSelfIntersections = (polyPoints) => {
+    let i, j;
+    let intersection = false;
+    for (i = 2; i < polyPoints.length; ++i)
+    {
+        for (j = 1; j < i - 1; ++j)
+        {
+            const edgeA = [polyPoints[i - 1], polyPoints[i]];
+            const edgeB = [polyPoints[j - 1], polyPoints[j]];
+            intersection = intersectEdges(edgeA, edgeB);
+            if (intersection)
+            {
+                break;
+            }
+        }
+        if (intersection)
+        {
+            break;
+        }
+    }
+
+    if (!intersection)
+    {
+        return polyPoints;
+    }
+
+    const polyA = [];
+    for (let k = 0; k < j; ++k)
+    {
+        polyA.push(polyPoints[k]);
+    }
+    polyA.push(intersection);
+    for (let k = i; k < polyPoints.length; ++k)
+    {
+        polyA.push(polyPoints[k]);
+    }
+
+    const polyB = [ intersection ];
+    for (let k = j; k < i; ++k)
+    {
+        polyB.push(polyPoints[k]);
+    }
+
+    if (manhPerimeter(polyA) > manhPerimeter(polyB))
+    {
+        return fixSelfIntersections(polyA);
+    }
+    return fixSelfIntersections(polyB);
+};
+
 const generateInnerCellContour = (polyPoints, polyCenter, desiredInnerMargin) => {
     const innerPolyPoints = [];
     for (let i = 0; i < polyPoints.length; ++i)
@@ -114,7 +222,7 @@ const generateInnerCellContour = (polyPoints, polyCenter, desiredInnerMargin) =>
 
           const cos = medianDir[0] * bc[0] + medianDir[1] * bc[1];
           const sin = Math.sqrt(1.0 - cos*cos);
-          const _innerMargin = desiredInnerMargin / sin;
+          const _innerMargin = desiredInnerMargin / Math.max(sin, 0.00000001);
 
           const candidateNumberOne = [b[0] + medianDir[0] * _innerMargin, b[1] + medianDir[1] * _innerMargin];
           const candidateNumberTwo = [b[0] - medianDir[0] * _innerMargin, b[1] - medianDir[1] * _innerMargin];
@@ -126,15 +234,19 @@ const generateInnerCellContour = (polyPoints, polyCenter, desiredInnerMargin) =>
 
           innerPolyPoints.push(innerPoint);
     }
-    return innerPolyPoints;
+
+    return fixSelfIntersections(innerPolyPoints);
 };
 
 const fillManhCellsLines = (manh) => {
     manh.forEach(mi => {
-        const innerPolyPoints = generateInnerCellContour(mi.polygonPoints, mi.site, innerCellRadiusMargin); // mewmew remove loops
+        const innerPolyPoints = generateInnerCellContour(mi.polygonPoints, mi.site, innerCellRadiusMargin);
+        const innerPolyPoints2 = generateInnerCellContour(innerPolyPoints, mi.site, innerCellRadiusMargin);
 
         addPolygonLines(mi.polygonPoints);
         addPolygonLines(innerPolyPoints);
+        //addPolygonLines(innerPolyPoints2);
+        // add shtrihovkas 45 to edge
     });
 };
 
@@ -142,11 +254,10 @@ const sketch = async ({ width, height, units, render }) => {
 
   const manh = generateManhattanVoronoi(width, height);
   fillManhNodesPoints(manh);
-  fillManhCellsLines(manh); // add shtrihovkas 45 to edge
+  fillManhCellsLines(manh);
 
   // 2. point circles to lines
-  // 3. remove overlapping lines in edges
-  // 4. sort draw order for plotter
+  // 3. robust draw order for plotter
 
 return ({ context }) => {
     context.clearRect(0, 0, width, height);
@@ -170,8 +281,6 @@ return ({ context }) => {
       context.lineCap = 'round';
       context.stroke();
     });
-
-
 
     if (debug.drawPoints) {
       debugPoints.forEach(p => {
